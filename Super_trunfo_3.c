cde23c9f57536acf7495a7e1ea670c7f486829a1
Leandro Silva <202501133134@alunos.estacio.br>
2025-04-10 19:05:56 -0300

    incluso if else

--- Super_trunfo_3.c
+++ Super_trunfo_3.c
@@ -5,7 +5,7 @@ int main()
     int opcao;
     printf("Super Trunfo!\n");
 
-    // Declaração e inicialização das variáveis para a primeira carta
+    // Carta 1
     char estado1;
     char codigo1[4];
     char cidade1[50];
@@ -17,14 +17,13 @@ int main()
     double divisaopib1;
     float super_poder1;
 
-    // Solicita os dados para a Carta 1
     printf("\n***** Carta 1 *****\n");
     printf("Digite o Estado (ex: A):\n ");
-    scanf(" %c", &estado1); // Espaço antes para limpar o buffer
+    scanf(" %c", &estado1);
     printf("Digite o código da carta (ex: A01):\n ");
     scanf("%s", codigo1);
     printf("Digite o nome da cidade (ex: São Paulo):\n ");
-    scanf(" %[^\n]", cidade1); // Captura até a nova linha
+    scanf(" %[^\n]", cidade1);
     printf("Digite a população (ex: 12325000):\n ");
     scanf("%lu", &populacao1);
     printf("Digite a área da cidade (ex: 1521.11):\n ");
@@ -35,10 +34,10 @@ int main()
     scanf("%d", &pontos1);
 
     densidade1 = (float)populacao1 / area1;
-    divisaopib1 = (pib1 * 1e9) / populacao1; // Convertendo PIB de bilhões para milhões
-    super_poder1 = (float)populacao1 + area1 + pib1 + pontos1 + (1 / densidade1) + divisaopib1;
+    divisaopib1 = (pib1 * 1e9) / populacao1;
+    super_poder1 = (float)populacao1 + area1 + pib1 + pontos1 + (1.0f / densidade1) + divisaopib1;
 
-    // Declaração e inicialização das variáveis para a segunda carta
+    // Carta 2
     char estado2;
     char codigo2[4];
     char cidade2[50];
@@ -50,17 +49,16 @@ int main()
     double divisaopib2;
     float super_poder2;
 
-    // Solicita os dados para a Carta 2
     printf("\n***** Carta 2 *****\n");
     printf("Digite o Estado (ex: B):\n ");
-    scanf(" %c", &estado2); // Espaço antes para limpar o buffer
+    scanf(" %c", &estado2);
     printf("Digite o código da carta (ex: B02):\n ");
     scanf("%s", codigo2);
-    printf("Digite o nome da cidade(ex: Rio de Janeiro):\n ");
-    scanf(" %[^\n]", cidade2); // Captura até a nova linha
+    printf("Digite o nome da cidade (ex: Rio de Janeiro):\n ");
+    scanf(" %[^\n]", cidade2);
     printf("Digite a população (ex: 6748000):\n ");
     scanf("%lu", &populacao2);
-    printf("Digite a área da cidade (ex:1200.25):\n ");
+    printf("Digite a área da cidade (ex: 1200.25):\n ");
     scanf("%f", &area2);
     printf("Digite o PIB da cidade (ex: 300.50):\n ");
     scanf("%f", &pib2);
@@ -68,11 +66,22 @@ int main()
     scanf("%d", &pontos2);
 
     densidade2 = (float)populacao2 / area2;
-    divisaopib2 = (pib2 * 1e9) / populacao2; // Convertendo PIB de bilhões para milhões
-    super_poder2 = (float)populacao2 + area2 + pib2 + pontos2 + (1 / densidade2) + divisaopib2;
+    divisaopib2 = (pib2 * 1e9) / populacao2;
+    super_poder2 = (float)populacao2 + area2 + pib2 + pontos2 + (1.0f / densidade2) + divisaopib2;
 
-    // Comparação automática de um atributo escolhido no código
+    // Escolha do atributo
     printf("\n***** Comparação *****\n");
+    printf("%s contra %s\n", cidade1, cidade2);
+
+    printf("\nEscolha o atributo para comparar:\n");
+    printf("1 - População\n");
+    printf("2 - Área\n");
+    printf("3 - PIB\n");
+    printf("4 - Pontos Turísticos\n");
+    printf("5 - Densidade Demográfica\n");
+    printf("6 - PIB per capita\n");
+    printf("Digite o número da opção:\n");
+    scanf("\n%d", &opcao);
 
     switch (opcao)
     {
@@ -151,6 +160,17 @@ int main()
     default:
         printf("Opção inválida! Por favor, selecione um número entre 1 e 6.\n");
     }
+
+    // Super Poder
+    printf("\n***** Super Poder *****\n");
+    printf("%s: %.2f\n", cidade1, super_poder1);
+    printf("%s: %.2f\n", cidade2, super_poder2);
+    if (super_poder1 > super_poder2)
+        printf("Super Trunfo: %s venceu!\n", cidade1);
+    else if (super_poder2 > super_poder1)
+        printf("Super Trunfo: %s venceu!\n", cidade2);
+    else
+        printf("Empate no Super Poder!\n");
 
     return 0;
 }
